5ba2926d72144b5483d01a22219d0bcc824f90df
Sam Li <samlzx96@gmail.com>
2019-08-09 15:59:37 -0400

    forms

---
+++ fire.js
@@ -0,0 +1,18 @@
+import firebase from "firebase";
+import "@firebase/firestore";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDeRhE5XKdDTo6yLQGYTDdbvohpjdjH218",
+  authDomain: "not-a-race.firebaseapp.com",
+  databaseURL: "https://not-a-race.firebaseio.com",
+  projectId: "not-a-race",
+  storageBucket: "",
+  messagingSenderId: "599976808896",
+  appId: "1:599976808896:web:54d9570d452d6118"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export const firestore = firebase.firestore();
+export default firebase;
