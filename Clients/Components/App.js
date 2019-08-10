8f5cb2309620b7cd0fb2ac391a2a6040c8405a62
rm1221 <41651067+rm1221@users.noreply.github.com>
2019-08-10 18:47:34 -0400

    unfinished group component

--- Clients/Components/App.js
+++ Clients/Components/App.js
@@ -6,12 +6,16 @@ import SignUp from './SignUp';
 import Loading from './Loading';
 import LogIn from './Login';
 import UserPage from './UserPage';
+import Groups from './Groups'
+import AddGroupForm from './AddGroupForm';
 
 const MainNavigator = createStackNavigator({
   Loading: { screen: Loading },
   Signup: { screen: SignUp },
   Login: { screen: LogIn },
-  UserPage: { screen: UserPage }
+  UserPage: { screen: UserPage },
+  Groups: { screen: Groups },
+  AddGroupForm: {screen: AddGroupForm}
 });
 
 const App = createAppContainer(MainNavigator);
--- Clients/Components/Groups.js
+++ Clients/Components/Groups.js
@@ -1,23 +1,27 @@
 import React, { Component } from 'react';
-import { SafeAreaView, Text, View, TextInput, Button } from 'react-native';
+import UserPage from './UserPage';
+import {
+  StyleSheet,
+  SafeAreaView,
+  Text,
+  View,
+  TextInput,
+  Button
+} from 'react-native';
 import styles from './Style';
 import { firestore } from '../../fire';
 
-export default class UserPage extends Component {
-    static navigationOptions = {
-        title: 'Welcome'
-      };
-    
+export default class Groups extends Component {
   render() {
     const { navigate } = this.props.navigation;
     return (
       <View style={styles.container}>
-        <Text style={styles.userPage}>Welcome</Text>
+        <Text style={styles.userPage}>Groups</Text>
         <Button
           onPress={() => {
-            navigate('Groups');
+            navigate('AddGroupForm');
           }}
-          title="Groups"
+          title="Add a group"
           color="white"
         />
       </View>
