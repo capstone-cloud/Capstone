902fd7eb2c020b3a4be9cce3cbacc8043ac22b46
rm1221 <41651067+rm1221@users.noreply.github.com>
2019-08-10 22:10:06 -0400

    almost done groups

--- Clients/Components/Groups.js
+++ Clients/Components/Groups.js
@@ -8,21 +8,58 @@ import {
   TextInput,
   Button
 } from 'react-native';
+import { ListItem, Card } from 'react-native-elements';
 import styles from './Style';
 import { firestore } from '../../fire';
 
 export default class Groups extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      groups: []
+    };
+    this.ref = firestore
+      .collection('publicUsers')
+      .doc('rameen98')
+      .collection('groups');
+  }
+  componentDidMount() {
+    this.ref
+      .get()
+      .then(snapshot => {
+        const groups = [];
+        snapshot.forEach(doc => {
+          console.log(doc.id, '=>', doc.data());
+          groups.push(doc.data());
+        });
+        this.setState({ groups: groups });
+      })
+      .catch(err => {
+        console.log('Error', err);
+      });
+  }
+
   render() {
     const { navigate } = this.props.navigation;
+    console.log(this.state);
     return (
-      <View style={styles.container}>
+      <View>
         <Text style={styles.userPage}>Groups</Text>
+        <Card title="GROUPS">
+          {this.state.groups.map((group, i) => (
+            <ListItem
+              key={i}
+              title={group.groupname}
+              subtitle={group.members}
+            />
+          ))}
+        </Card>
         <Button
           onPress={() => {
             navigate('AddGroupForm');
           }}
           title="Add a group"
-          color="white"
+          color="black"
         />
       </View>
     );
