5d9e7424bbc8dfe55a91131f0175400f8a008188
Sam Li <samlzx96@gmail.com>
2019-08-17 15:59:23 -0400

    chatcomplete

--- Clients/Components/Chat.js
+++ Clients/Components/Chat.js
@@ -1,53 +1,63 @@
+import React, { Component } from "react";
+import { firestore, auth } from "../../fire";
+import { GiftedChat } from "react-native-gifted-chat";
 
-import React, { Component } from 'react';
-import {
-  SafeAreaView,
-  Text,
-  TouchableOpacity,
-  TextInput,
-  FlatList,
-  Dimensions,
-  View
-} from 'react-native';
-import firebase, { firestore, auth } from '../../fire';
-import { GiftedChat } from 'react-native-gifted-chat';
 class Chat extends Component {
   static navigationOptions = {
-    title: 'chattttttt'
+    title: "CHAT"
   };
   constructor(props) {
     super(props);
     this.state = {
-      user: this.props.navigation.getParam('user'),
+      user: this.props.navigation.getParam("user"),
       messages: []
     };
     this.ref = firestore
-      .collection('chat')
-      .doc(this.props.navigation.getParam('groupId'));
-    this.groupname = this.props.navigation.getParam('groupname');
+      .collection("chat")
+      .doc(this.props.navigation.getParam("groupId"));
   }
   componentDidMount() {
     this.ref.onSnapshot(doc => {
-      this.setState({
-        messages: doc.data().chits
+      const messages = [];
+      doc.data().chits.forEach(chit => {
+        let date = new Date(chit.createdAt.seconds * 1000);
+        messages.push({
+          _id: chit._id,
+          createdAt: date,
+          text: chit.text,
+          user: chit.user
+        });
+      });
+      this.setState(previousState => {
+        return {
+          messages: GiftedChat.append([], messages)
+        };
       });
     });
   }
+
   onSend = messages => {
     const { _id, createdAt, text, user } = messages[0];
-    const message = { _id, timestamp: null, text, user };
+    const message = {
+      _id,
+      createdAt: new Date(),
+      text,
+      user
+    };
     this.ref.update({ chits: [...this.state.messages, message] });
   };
   render() {
     return (
       <GiftedChat
-        messages={this.state.messages}
+        messages={[...this.state.messages].reverse()}
         onSend={messages => this.onSend(messages)}
-        user={{ _id: auth.currentUser.uid, name: this.state.user, avatar: '' }}
-        inverted={false}
+        user={{ _id: auth.currentUser.uid, name: this.state.user, avatar: "" }}
+        renderTime={() => {
+          return null;
+        }}
       />
     );
   }
 }
-export default Chat;
 
+export default Chat;
